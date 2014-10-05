a1a3cfba1b6f05dad70047abe77e0fd18bcff8ff
sotirisnik <sotirisnik@gmail.com>
2014-10-05 20:26:10 +0300

    solve_me_first

---
+++ solve_me_first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int a, b;
+
+int main( ) {
+    
+    scanf( "%d %d", &a, &b );
+    
+    printf( "%d\n", a + b );
+    
+    return 0;
+
+}
